100fda0ddcbefe7da94854752469b31639d9b3c3
Clovis Durand <cd.clovel19@gmail.com>
2020-02-27 10:26:03 +0100

    Added socket related internal variables
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

--- src/can_ip_private.h
+++ src/can_ip_private.h
@@ -10,17 +10,29 @@
 /* Includes -------------------------------------------- */
 #include "can_ip.h"
 
+#include <netinet/in.h>
+
 #include <stdint.h>  /* TODO : Delete this and use custom types */
 #include <stdbool.h> /* TODO : Delete this and use custom types */
 
 /* Defines --------------------------------------------- */
 
 /* Type definitions ------------------------------------ */
+typedef int cipSocket_t;
+
 typedef struct _cipInternalVariables {
     uint8_t   cipInstanceID; /* TODO : Multiline CAN */
     cipMode_t cipMode;
     bool      isInitialized;
     bool      isStopped;
+
+    /* sockeS */
+    cipSocket_t         canSocket; /* The socket used to communicate CAN frames */
+    struct sockaddr_in  socketInAddress;
+    char               *canIP;      /* IP Address */
+    cipPort_t           canPort;    /* Server port number */
+    struct hostent     *hostPtr;    /* Server information */
+    struct addrinfo    *addrinfo;   /* Address information fetched w/ getaddrinfo */
 } cipInternalStruct_t;
 
 #endif /* CAN_IP_PRIVATE_H */

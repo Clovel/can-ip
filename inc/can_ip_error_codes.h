24179325beebf45ca1c3404953519761490e6960
Clovis Durand <cd.clovel19@gmail.com>
2020-02-27 09:37:55 +0100

    Error codes now public + Updated + Added CONFIG error code
    Signed-off-by: Clovis Durand <cd.clovel19@gmail.com>

---
+++ inc/can_ip_error_codes.h
@@ -0,0 +1,27 @@
+/**
+ * @brief CAN over IP error codes
+ * 
+ * @file can_ip_error_codes.h
+ */
+
+#ifndef CAN_IP_ERROR_CODES_H
+#define CAN_IP_ERROR_CODES_H
+
+/* Includes -------------------------------------------- */
+
+/* Defines --------------------------------------------- */
+
+/* Type definitions ------------------------------------ */
+typedef enum _cipErrorCodes {
+    CAN_IP_ERROR_UNKNOWN      = 0U,
+    CAN_IP_ERROR_NONE         = 1,
+    CAN_IP_ERROR_ARG          = 2,
+    CAN_IP_ERROR_SYS          = 3,
+    CAN_IP_ERROR_NET          = 4,
+    CAN_IP_ERROR_ALREADY_INIT = 5,
+    CAN_IP_ERROR_NOT_INIT     = 6,
+    CAN_IP_ERROR_STOPPED      = 7,
+    CAN_IP_ERROR_CONFIG       = 8
+} cipErrorCode_t;
+
+#endif /* CAN_IP_ERROR_CODES_H */
